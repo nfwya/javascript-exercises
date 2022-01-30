1cc68bb0c21449738becdb1672932adcc71f9712
Doc_husky <nfwya0811@gmail.com>
2022-01-30 10:52:27 +0800

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let resultArray = array;
+    for(let i = 1; i< arguments.length ; i ++ ){
+        console.log(arguments[i]);
+        index = resultArray.indexOf(arguments[i]);
+        console.log(index)
+        if(index !== -1 ){
+            resultArray.splice(index,1);
+        }
+        console.log(resultArray)
+    };
+    return resultArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
