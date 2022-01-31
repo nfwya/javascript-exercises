2f5c150aa9a94dc45acb4f10810d2f0594276098
Doc_husky <nfwya0811@gmail.com>
2022-01-31 09:48:12 +0800

    to 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
+const add = function(num1,num2) {
+  if(typeof(num1) !== 'number' ||
+  typeof(num2) !== 'number'){
+    return 'ERROR';
+  }
+  return num1 + num2 ;
+};
+
+const subtract = function(num1,num2) {
+  if(typeof(num1) !== 'number' ||
+  typeof(num2) !== 'number'){
+    return 'ERROR';
+  }
+  return num1 - num2;
+};
+
+const sum = function(arr) {
+	return ( arr.reduce(
+    (acc,curr,index,arr) => {return acc + curr}, 0) );
+};
+
+const multiply = function(arr) {
+  return ( arr.reduce(
+    (acc,curr,ind,arr) => { return acc * curr}, 1
+  ));
+
+};
+
+const power = function(base, power) {
+  let result = 1;
+  for(let i=0; i<power; i++){
+    result = result * base;
+  }
+  return result;
+};
+
+const factorial = function(num) {
+  let result = 1;
+  for(let i=1; i<=num ; i++){
+    result = result * i;
+  }
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+//console.log( multiply([1,2,3,4]) );
+//console.log(power(3,4))
+//console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let trueStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    trueStr = trueStr.replace(/\s/g,'');
+    const reverse = (str) =>{
+        let result = '';
+        for(i=str.length-1; i>=0; i--){
+            result = result + str[i];
+        }
+        return result;
+    };
+    const reverseStr = reverse(trueStr);
+    return reverseStr.toLowerCase() === trueStr.toLowerCase();
 };
+
+console.log(palindromes("A!#$%^&nimal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
