917f48c33670b45b8270db264e7e3b1c61898a56
Doc_husky <nfwya0811@gmail.com>
2022-01-31 10:21:59 +0800

    finished foundation 5

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return 'OOPS';
+    }
+    let current = 1;
+    let previous = 0;
+    let previous_two = 0;
+    for(i=1; i<num; i++){
+        let tmpCurrent = current;
+        current = previous + current;
+        previous_two = previous;
+        previous = tmpCurrent;
+    }
+    return current;
 };
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return (
+        people.reduce(
+            (acc, curr, index, arr) =>{
+                const get_age = (person) =>{
+                    return (typeof( person.yearOfDeath ) === 'number')?(person.yearOfDeath - person.yearOfBirth): (new Date().getFullYear() - person.yearOfBirth)
+                };
+                return (get_age(acc) > get_age(curr) )? acc : curr;
+            }
+            ,people[0]
+        )
+    )
 };
 
+/*
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+console.log(findTheOldest(people).name);
+*/
 // Do not edit below this line
 module.exports = findTheOldest;
